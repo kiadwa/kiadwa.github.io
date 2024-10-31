d8450506cb667966ffb286cb254aa6b205155d00
Ky Anh <phamhuykyanh90502@gmail.com>
2024-10-31 13:02:34 +1100

    configure for build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Lab-01-Group-06/"
+  base: "/kiadwa.github.io/",
+  build:{
+    target: 'esnext',
+  }
+
+
 })
